3a12a68af0138c97a316382658e942a88c62e40d
Karinarchmdini22 <karinarchmdini@gmail.com>
2025-06-02 19:27:53 +0700

    Mengupdate tampilan wpu-hut

--- wpu-hut/latihan1.php
+++ wpu-hut/latihan1.php
@@ -54,7 +54,7 @@ $menu = $menu['menu'];
                  <h5 class="card-title"><?= $row["nama"]; ?></h5>
                  <p class="card-text"><?= $row["deskripsi"]; 
                  ?></p>
-                 <h5 class="card-title">Rp. <?= $row["harga"]
+                 <h5 class="card-title">Rp. <?= number_format( $row["harga"], 0, ',', '.')
                  ; ?></h5>
                  <a href="#" class="btn btn-primary">Pesan
                  Sekarang</a>
